c2b211f106b806efeb2667117c4f527235ef7ebd
dnoseda <damian.noseda@mercadolibre.com>
2011-06-27 15:04:56 -0300

    new version

--- NewrelicGrailsPlugin.groovy
+++ NewrelicGrailsPlugin.groovy
@@ -1,7 +1,7 @@
 import com.newrelic.api.agent.NewRelic
 class NewrelicGrailsPlugin {
 	// the plugin version
-	def version = "0.3"
+	def version = "0.4"
 	// the version or versions of Grails the plugin is designed for
 	def grailsVersion = "1.3.5 > *"
 	// the other plugins this plugin depends on
